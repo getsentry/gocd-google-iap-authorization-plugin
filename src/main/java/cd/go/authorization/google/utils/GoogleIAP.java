fc9ac1b9c57f8a9931f0bc86b298f8e614a84e49
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2023-02-22 12:17:53 -0800

    Add naive checking

--- src/main/java/cd/go/authorization/google/utils/GoogleIAP.java
+++ src/main/java/cd/go/authorization/google/utils/GoogleIAP.java
@@ -3,7 +3,17 @@ package cd.go.authorization.google.utils;
 import cd.go.authorization.google.requests.Request;
 
 public class GoogleIAP {
+  private static String JWT_HEADER_KEY = "X-Goog-IAP-JWT-Assertion";
+
   public static boolean isValidIAPRequest(final Request request) {
-    return false;
+    String jwtHeader = request.requestHeaders().get(JWT_HEADER_KEY);
+    if (jwtHeader == null || jwtHeader == "") {
+      return false;
+    }
+
+    System.out.println("JWT Header ==> " + jwtHeader);
+    System.out.println("TODO: Validate the JWT");
+
+    return true;
   }
 }
